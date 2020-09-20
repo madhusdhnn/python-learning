8126e0f3b76151a6a58edb5a0841a0a5c2957f67
madhusdhnn <madhu.padhmanaban298@gmail.com>
2020-09-21 00:34:31 +0530

    Introduce python

---
+++ app.py
@@ -0,0 +1,13 @@
+print("Hello world")
+
+int_var = 24
+print(int_var)
+
+float_var = 24.00343423
+print(float_var)
+
+string_var = "A String. Wow"
+print(string_var)
+
+bool_var = False
+print(bool_var)
